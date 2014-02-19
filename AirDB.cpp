6642be2dc66249704cbf72b8cd8d217f74334bfe
jcwearn <jacksonwearn@gmail.com>
2014-02-18 21:57:30 -0500

    simple node implemented

--- AirDB.cpp
+++ AirDB.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
-#include "Node.cpp"
 using namespace std;
+
+typedef struct node {
+  int data;
+  node *next;
+};
+
+int main(void) {
+  int input;
+  node *head = NULL;  //empty linked list
+  cout << "Enter some data";
+  cin >> input;
+
+  node *temp;
+  temp = new node;
+
+  temp->data = input;
+  temp->next = head;
+  head = temp;
+  
+  cout << head->data;
+}
+
+
