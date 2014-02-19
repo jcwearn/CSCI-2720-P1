8ab51901fd9209bf569685ad39fc370219c96fe8
jcwearn <jacksonwearn@gmail.com>
2014-02-18 22:41:22 -0500

    Added skeleton of methods from assignment pdf

--- AirDB.cpp
+++ AirDB.cpp
@@ -1,25 +1,77 @@
 #include <iostream>
 using namespace std;
 
-typedef struct node {
+ struct node {
   int data;
   node *next;
 };
 
-int main(void) {
-  int input;
-  node *head = NULL;  //empty linked list
-  cout << "Enter some data";
-  cin >> input;
+node * head = NULL;
 
-  node *temp;
-  temp = new node;
+/*
+ * Add a new flight to the list of flights
+ *
+ */
+void addFlight(int flightNo) {
+  int *temp;
+  tmep = new node;
 
-  temp->data = input;
+  temp->data = flightNo;
   temp->next = head;
-  head = temp;
-  
-  cout << head->data;
+
+  head = temp;  
 }
 
+/*
+ * Delete a list from the list of flights
+ *
+ */
+void deleteFlight(int flightNo) {
+}
 
+/*
+ * Add a passenger to a flight
+ *
+ */
+void addPassenger(int flightNo, string lastName, string firstName, int seatNo) {
+}
+
+/*
+ * remove a passenger from a flight
+ *
+ */
+void removePassenger(int flightNo, string lastName, string firstName, int seatNo) {
+}
+
+/*
+ * Print out the passenger information including seat number.
+ *
+ */
+void showPassenger(int flightNo, string lastName, string firstName) {
+}
+
+/*
+ * Print out all the flight numbers
+ *
+ */
+void showAllFlights() {
+}
+
+/*
+ * Print out all the passengers in a flight
+ *
+ */
+void showAllPassengers(int flightNo) {
+}
+
+/*
+ * Print out all flight numbers and all passengers in each flight
+ *
+ */
+void showAllFlightsAndPassengers() {
+}
+
+int main(void) {
+  addFlight();
+  
+}
