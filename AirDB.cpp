d5c2e010c82506eff6bba676fd215c8651e8af38
jcwearn <jacksonwearn@gmail.com>
2014-02-25 22:54:57 -0500

    changed node to class rather than struct

--- AirDB.cpp
+++ AirDB.cpp
@@ -1,77 +1,87 @@
 #include <iostream>
 using namespace std;
 
- struct node {
+class AirDB {
+protected:
+  AirDB * next;
   int data;
-  node *next;
+public:
+  void addFlight(int);
+  void deleteFlight(int);
+  void addPassenger(int, string, string, int);
+  void removePassenger(int, string, string, int);
+  void showPassenger(int, string, string);
+  void showAllFlights();
+  void showAllPassengers(int);
+  void showAllFlightsAndPassengers();
 };
-
-node * head = NULL;
 
 /*
  * Add a new flight to the list of flights
  *
  */
-void addFlight(int flightNo) {
-  int *temp;
-  tmep = new node;
-
-  temp->data = flightNo;
-  temp->next = head;
-
-  head = temp;  
+void AirDB::addFlight(int flightNo) {
+  //int *temp;
+  //temp = new node;
+    
+  //temp->data = flightNo;
+  //temp->next = head;
+  
+  //head = temp;  
 }
 
 /*
  * Delete a list from the list of flights
  *
  */
-void deleteFlight(int flightNo) {
+void AirDB::deleteFlight(int flightNo) {
 }
 
 /*
  * Add a passenger to a flight
  *
  */
-void addPassenger(int flightNo, string lastName, string firstName, int seatNo) {
+void AirDB::addPassenger(int flightNo, string lastName, string firstName, int seatNo) {
 }
 
 /*
  * remove a passenger from a flight
  *
  */
-void removePassenger(int flightNo, string lastName, string firstName, int seatNo) {
+void AirDB::removePassenger(int flightNo, string lastName, string firstName, int seatNo) {
 }
 
 /*
  * Print out the passenger information including seat number.
  *
  */
-void showPassenger(int flightNo, string lastName, string firstName) {
+void AirDB::showPassenger(int flightNo, string lastName, string firstName) {
 }
 
 /*
  * Print out all the flight numbers
  *
  */
-void showAllFlights() {
+void AirDB::showAllFlights() {
 }
 
 /*
  * Print out all the passengers in a flight
  *
  */
-void showAllPassengers(int flightNo) {
+void AirDB::showAllPassengers(int flightNo) {
 }
 
 /*
  * Print out all flight numbers and all passengers in each flight
  *
  */
-void showAllFlightsAndPassengers() {
+void AirDB::showAllFlightsAndPassengers() {
 }
 
 int main(void) {
-  addFlight();
+
+
+
   
 }
